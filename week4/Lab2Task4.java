9f51fb38710e01e6bab76cd48ac2150b22fb4130
lyd-2004 <3343109033@qq.com>
2024-10-05 13:59:59 +0800

    Update Lab2Task4.java

--- week4/Lab2Task4.java
+++ week4/Lab2Task4.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class Cylinder {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the radius and length of a cylinder: ");        
         double radius=sc.nextDouble();
         double length=sc.nextDouble();
         double area = Math.PI * radius * radius;
         double volume = area * length;
-        System.out.println(area);
-        System.out.println(volume);
-
+        System.out.println("The area is "+area);
+        System.out.println("The volume is "+volume);
     }
 }
