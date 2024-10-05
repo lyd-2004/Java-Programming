4982284bbf8eb7f3a323f65f86628f2108c010ce
lyd-2004 <3343109033@qq.com>
2024-10-05 21:34:11 +0800

    Update Lab2Task18.java

--- week4/Lab2Task18.java
+++ week4/Lab2Task18.java
@@ -1,1 +1,21 @@
-
+import java.util.Scanner;
+public class Determing_Leap_Year2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year=sc.nextInt();
+        boolean isLeapYear=(year%4==0) && (year%100!=0);
+        if(!isLeapYear) {
+            System.out.println(year+" isn't a leap year.);
+        }else{
+            System.out.println(year+" is a leap year.);
+        }
+        isLeapYear=(year%400==0) && (year%100==0);
+        if(!isLeapYear) {
+            System.out.println(year+" isn't a leap year.);
+        }else{
+            System.out.println(year+" is a leap year.);
+        }
+        System.out.println(year + " is a leap year? " + isLeapYear);
+    }
+}
