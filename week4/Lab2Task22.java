c47bc8e972a45574f468fbcfd3988ea2fe30ecd5
lyd-2004 <3343109033@qq.com>
2024-10-05 21:48:35 +0800

    Update Lab2Task22.java

--- week4/Lab2Task22.java
+++ week4/Lab2Task22.java
@@ -1,1 +1,23 @@
-
+import java.util.Scanner;
+public class What_is_x {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of x");
+        int x=sc.nextInt();
+        System.out.println("Enter the value of a");
+        int a=sc.nextInt();
+        switch(a) {
+            case 1:
+                x+=5;
+                break;
+            case 2:
+                x+=10;
+            case 3:
+                x+=16;
+            case 4:
+                x+=34;
+        }
+        System.out.println("The value of x is "+ x);
+        System.out.println("The value of a is "+ a);
+    }
+}
