65c1c1dc732844bdeb4833e7e7b50060168cff9d
lyd-2004 <3343109033@qq.com>
2024-10-05 17:02:05 +0800

    Update Lab2Task12.java

--- week4/Lab2Task12.java
+++ week4/Lab2Task12.java
@@ -1,1 +1,14 @@
-
+import java.util.Scanner;
+public class HiFive_or_HiEven {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number=input.nextInt();
+        if(number%5==0) {
+            System.out.println("HiFive");
+        }
+        if(number%2==0) {
+            System.out.println("HiEven");
+        }
+    }
+}
