229a8e0c555b618e60e0ca75ebecafc31f2d3cc0
lyd-2004 <3343109033@qq.com>
2024-10-06 00:15:22 +0800

    Update and rename Math_Pow.java to ExponentOperation.java

--- week3/ExponentOperation.java
+++ week3/ExponentOperation.java
@@ -1,4 +1,4 @@
-public class Math_Pow {
+public class ExponentOperation {
     public static void main(String[] args) {
         System.out.println(Math.pow(2,3));
         System.out.println(Math.pow(4,0.5));
