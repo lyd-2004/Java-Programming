6c8790d56ddd88e240c7d289bd2ac50b76e2be65
lyd-2004 <3343109033@qq.com>
2024-10-06 00:11:01 +0800

    Update and rename Example_3_1 to AdditionQuiz.java

--- week3/AdditionQuiz.java
+++ week3/AdditionQuiz.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Example_3_1 {
+public class AdditionQuiz {
     public static void main(String[] args) {
         int number1=(int)(System.currentTimeMillis()%10);
         int number2=(int)(System.currentTimeMillis()/10%10);
