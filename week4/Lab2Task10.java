59c09a59c2b0b9bdd2314765b8d63262c6e85556
lyd-2004 <3343109033@qq.com>
2024-10-05 16:51:15 +0800

    Update Lab2Task10.java

--- week4/Lab2Task10.java
+++ week4/Lab2Task10.java
@@ -1,1 +1,13 @@
-
+import java.util.Scanner;
+public class Financial_Compound' {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the monthly saving amount: ");
+        double saving=sc.nextDouble();
+        double value=0;
+        for(int i=1;i<=6;i++) {
+            value=(value+saving)*(1+0.00417);
+        }
+        System.out.println("After the sixth month, the account value is $"+value);
+    }
+}
