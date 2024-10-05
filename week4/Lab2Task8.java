63c3ebba25bae313555e3a463791cf61ce2f78fa
lyd-2004 <3343109033@qq.com>
2024-10-05 15:21:03 +0800

    Update Lab2Task8.java

--- week4/Lab2Task8.java
+++ week4/Lab2Task8.java
@@ -1,1 +1,10 @@
-
+import java.util.Scanner;
+public class Hexagon {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the length of the side: ");
+        double length=sc.nextDouble();
+        double area=3*Math.sqrt(3)/2*length*length;
+        System.out.println("The area of the hexagon is "+area);
+    }
+}
