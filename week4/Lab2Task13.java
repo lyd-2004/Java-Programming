3453a21d74cca9adad16add2ad127d5404cadccd
lyd-2004 <3343109033@qq.com>
2024-10-05 17:13:57 +0800

    Update Lab2Task13.java

--- week4/Lab2Task13.java
+++ week4/Lab2Task13.java
@@ -1,1 +1,17 @@
-
+import java.util.Scanner;
+public class Pay {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your pay: ");
+        double pay=sc.nextDouble();
+        System.out.println("Your pay without increment is: "+pay);
+        System.out.print("Enter your score: ");
+        double score=sc.nextDouble();
+        if(score>=90) {
+            pay=pay*1.03;
+        }else{
+            pay=pay*1.01;
+        }
+        System.out.println("your pay with increment is " +pay);
+    }
+}
