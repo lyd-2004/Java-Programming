295d4f424a7c947ac0c0dde0feff702b336e74c4
lyd-2004 <3343109033@qq.com>
2024-10-06 00:06:13 +0800

    Update and rename Example_3_2 to SimpleIfDemo.java

--- week3/SimpleIfDemo.java
+++ week3/SimpleIfDemo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Example_3_2 {
+public class SimpleIfDemo {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.print("Enter an integer: ");
