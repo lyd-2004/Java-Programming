06529540745e496ca00e19174daec76a12bd66b6
lyd-2004 <3343109033@qq.com>
2024-10-05 13:55:55 +0800

    Update Lab2Task4.java

--- week4/Lab2Task4.java
+++ week4/Lab2Task4.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
+public class Cylinder {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double radius=sc.nextDouble();
+        double length=sc.nextDouble();
+        double area = Math.PI * radius * radius;
+        double volume = area * length;
+        System.out.println(area);
+        System.out.println(volume);
 
+    }
+}
