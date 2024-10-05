44c09da583f7b982c435127b5025d2493897445d
lyd-2004 <3343109033@qq.com>
2024-10-05 19:17:57 +0800

    Update Lab2Task10.java

--- week4/Lab2Task10.java
+++ week4/Lab2Task10.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Financial_Compound' {
+public class Financial_Compound2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the monthly saving amount: ");
