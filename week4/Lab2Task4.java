c5ef7aa847cb24d5037842240b206bad2e9a2612
lyd-2004 <3343109033@qq.com>
2024-10-05 15:03:44 +0800

    Update Lab2Task4.java

--- week4/Lab2Task4.java
+++ week4/Lab2Task4.java
@@ -5,8 +5,8 @@ public class Cylinder {
         System.out.print("Enter the radius and length of a cylinder: ");        
         double radius=sc.nextDouble();
         double length=sc.nextDouble();
-        double area = Math.PI * radius * radius;
-        double volume = area * length;
+        double area=Math.PI * radius * radius;
+        double volume=area * length;
         System.out.println("The area is "+area);
         System.out.println("The volume is "+volume);
     }
