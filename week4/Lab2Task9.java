d4dc5f2688649723900bb1559c3fe39576dba0aa
lyd-2004 <3343109033@qq.com>
2024-10-05 15:40:09 +0800

    Update Lab2Task9.java

--- week4/Lab2Task9.java
+++ week4/Lab2Task9.java
@@ -1,1 +1,11 @@
-
+public class Financial_Compound {
+    public static void main(String[] args) {
+        String[] arr={"first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth"};
+        double saving=100.0;
+        double value=0;
+        for(int i=0;i<arr.length;i++) {
+            value=(value+saving)*(1+0.00417);
+            System.out.println("After the "+arr[i]+" month,the value in the account becomes "+value);
+        }
+    }
+}
