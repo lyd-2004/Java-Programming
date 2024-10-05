74bf1eac0e393fe845efd1d17b3125e67df1299b
lyd-2004 <3343109033@qq.com>
2024-10-05 21:51:21 +0800

    Update Lab2Task23.java

--- week4/Lab2Task23.java
+++ week4/Lab2Task23.java
@@ -1,1 +1,12 @@
-
+import java.util.Scanner;
+public class ConditionalOperator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter num1:");
+        int num1=sc.nextInt();
+        System.out.println("Enter num2:");
+        int num2=sc.nextInt();
+        int max=(num1>num2)?num1:num2;
+        System.out.println(max);
+    }
+}
