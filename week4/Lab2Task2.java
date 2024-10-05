6fae8f83079d6ebea4140914f8a0f5e5c2d40b66
lyd-2004 <3343109033@qq.com>
2024-10-05 13:23:56 +0800

    Update Lab2Task2.java

--- week4/Lab2Task2.java
+++ week4/Lab2Task2.java
@@ -1,1 +1,11 @@
-
+import java.util.Scanner;
+public class DisplayTime {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int seconds=sc.nextInt();
+        int minutes=seconds/60;
+        int remainingSeconds=seconds%60;
+        System.out.println(minutes);
+        System.out.println(remainingSeconds);
+    }
+}
