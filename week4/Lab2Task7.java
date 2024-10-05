5eb21d57230f622a7c3290fc71aa8aa0301b693d
lyd-2004 <3343109033@qq.com>
2024-10-05 15:06:58 +0800

    Update Lab2Task7.java

--- week4/Lab2Task7.java
+++ week4/Lab2Task7.java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+public class Driving {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the driving distance:");
+        double drivingDistance=sc.nextDouble();
+        System.out.println("Enter miles per gallon:");
+        double milesPerGallon = sc.nextDouble();
+        System.out.println("Enter price per gallon:");
+        double pricePerGallon = sc.nextDouble();
+        double cost = drivingDistance / milesPerGallon * pricePerGallon;
+        System.out.print("The cost of driving is $");
+        System.out.printf("%.2f", cost);
+    }
+}
