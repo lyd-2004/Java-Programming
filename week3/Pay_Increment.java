df3363356ad990a75be0f2444eb876504b46c3e4
lyd-2004 <3343109033@qq.com>
2024-10-06 00:04:55 +0800

    Update and rename Example_3_3 to Pay_Increment.java

--- week3/Pay_Increment.java
+++ week3/Pay_Increment.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Example_3_3 {
+public class Pay_Increment {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.print("Enter your pay: ");
