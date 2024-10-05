790c0493766a691d06554fc88c94e40ac3822d40
lyd-2004 <3343109033@qq.com>
2024-10-05 16:54:10 +0800

    Update Lab2Task11.java

--- week4/Lab2Task11.java
+++ week4/Lab2Task11.java
@@ -1,1 +1,10 @@
-
+import java.util.Scanner;
+public class Sales_Tax {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter purchase amount: ");
+        double account=sc.nextDouble();
+        double tax=account*0.06;
+        System.out.println("Sales tax is $"+tax);
+    }
+}
