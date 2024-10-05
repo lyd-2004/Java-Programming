116af47b6c15c7d0e0cfb435dc4da96dddb6a598
lyd-2004 <3343109033@qq.com>
2024-10-05 21:47:05 +0800

    Update Lab2Task21.java

--- week4/Lab2Task21.java
+++ week4/Lab2Task21.java
@@ -1,1 +1,21 @@
-
+import java.util.Scanner;
+public class Weekday_or_Weekend {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the day number: ");
+        int day=sc.nextInt();
+        switch(day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday"); 
+                break;
+            case 6:
+            case 0:
+                System.out.println("Weekend"); 
+                break;
+        }
+    }
+}
