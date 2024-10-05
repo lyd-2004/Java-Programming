72d94e825aca1a065a0a184f68ebbc3c83e53a38
lyd-2004 <3343109033@qq.com>
2024-10-06 00:13:14 +0800

    Update and rename Example_2_5 to ReadingFromKeyboard.java

--- week3/ReadingFromKeyboard.java
+++ week3/ReadingFromKeyboard.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Example_2_5 {
+public class ReadingFromKeyboard {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.print("Enter a Byte value: ");
