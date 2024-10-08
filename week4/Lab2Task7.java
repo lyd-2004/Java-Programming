5c367b5336c89e1087066ab0a56f5d2560474976
lyd-2004 <3343109033@qq.com>
2024-10-08 16:38:50 +0800

    Update Lab2Task7.java

--- week4/Lab2Task7.java
+++ week4/Lab2Task7.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Driving {
+public class Lab2Task7 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the driving distance: ");
