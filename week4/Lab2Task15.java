60bc987c92edaa991061e3542e7675b6fc2f92ed
lyd-2004 <3343109033@qq.com>
2024-10-08 16:34:34 +0800

    Update Lab2Task15.java

--- week4/Lab2Task15.java
+++ week4/Lab2Task15.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Divisible {
+public class Lab2Task15 {
         public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             System.out.println("Enter a integer: ");
