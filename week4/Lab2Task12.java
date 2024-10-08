5517538087a6b765b7bce8e6b8825e0f1ff6aa22
lyd-2004 <3343109033@qq.com>
2024-10-08 16:33:52 +0800

    Update Lab2Task12.java

--- week4/Lab2Task12.java
+++ week4/Lab2Task12.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class HiFive_or_HiEven {
+public class Lab2Task12 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter an integer: ");
