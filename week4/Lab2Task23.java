5d85b8444c41fb94a2e75a128e5180c04a9a4148
lyd-2004 <3343109033@qq.com>
2024-10-08 16:36:46 +0800

    Update Lab2Task23.java

--- week4/Lab2Task23.java
+++ week4/Lab2Task23.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ConditionalOperator {
+public class Lab2Task23 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter num1:");
