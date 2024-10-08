7ae9688cf3d98a93eb70886d6d824e2028700ed4
lyd-2004 <3343109033@qq.com>
2024-10-08 16:37:13 +0800

    Update Lab2Task4.java

--- week4/Lab2Task4.java
+++ week4/Lab2Task4.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Cylinder {
+public class Lab2Task3 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the radius and length of a cylinder: ");        
