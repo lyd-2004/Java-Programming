997343b9ec4981785734a4b7cc17fcde47b59a90
lyd-2004 <3343109033@qq.com>
2024-10-08 16:34:06 +0800

    Update Lab2Task13.java

--- week4/Lab2Task13.java
+++ week4/Lab2Task13.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Pay {
+public class Lab2Task13 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter your pay: ");
