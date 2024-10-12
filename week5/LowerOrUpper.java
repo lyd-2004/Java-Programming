3930743d3ee7aadfda63cc05b8c5ff03bfa8111a
lyd-2004 <3343109033@qq.com>
2024-10-12 10:48:52 +0800

    Create LowerOrUpper.java

---
+++ week5/LowerOrUpper.java
@@ -0,0 +1,7 @@
+public class LowerOrUpper {
+    public static void main(String[] args) {
+        String message = "Welcome to Java";
+        System.out.println("The message in lowcase is : " + message.toLowerCase());
+        System.out.println("The message in uppercase is : " + message.toUpperCase());
+    }
+}
