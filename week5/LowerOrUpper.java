2bea581721884655210d360127745a5602c0f9b8
lyd-2004 <3343109033@qq.com>
2024-10-12 11:00:46 +0800

    Update and rename LowerOrUpper.java to ConvertingStrings.java

--- week5/LowerOrUpper.java
+++
@@ -1,7 +0,0 @@
-public class LowerOrUpper {
-    public static void main(String[] args) {
-        String message = "Welcome to Java";
-        System.out.println("The message in lowcase is : " + message.toLowerCase());
-        System.out.println("The message in uppercase is : " + message.toUpperCase());
-    }
-}
