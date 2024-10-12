991ad414f118b6a6abbd0b5a2ce34d13b504e2e6
lyd-2004 <3343109033@qq.com>
2024-10-12 11:23:10 +0800

    Create TaxRateWithFormating.java

---
+++ week5/TaxRateWithFormating.java
@@ -0,0 +1,10 @@
+public class TaxRateWithFormating {
+    public static void main(String[] args) {
+        double amount=12618.98;
+        double interestRate=0.0013;
+        double interst=amount*interestRate;
+        System.out.println("Interest is $"+interst);
+        System.out.printf("Interest with two decimal point is $%4.2f",interst);
+    }
+
+}
