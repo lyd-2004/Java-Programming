6c5802fa695d21cb846a7ba758581e1a1f5e955a
lyd-2004 <3343109033@qq.com>
2024-10-12 08:23:18 +0800

    Update and rename CODE 1.1 to myFirstJava.java

--- Chapter 1/myFirstJava.java
+++ Chapter 1/myFirstJava.java
@@ -1,4 +1,4 @@
-public class App {
+public class myFirstJava {
   public static void main(String[] args) {
     System.out.println("Welcome to Java");
     System.out.println("Welcome to Computer Science");
