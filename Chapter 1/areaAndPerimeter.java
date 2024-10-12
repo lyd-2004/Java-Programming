582d2e7b654b1f2f2b2cac6ce06852a518cc814b
lyd-2004 <3343109033@qq.com>
2024-10-12 09:05:36 +0800

    Update and rename CODE 1.9 to areaAndPerimeter.java

--- Chapter 1/areaAndPerimeter.java
+++ Chapter 1/areaAndPerimeter.java
@@ -1,12 +1,12 @@
-public class App {
+public class areaAndPerimeter {
   public static void main(String[] args) {
     Scanner input=new Scanner(System.in);
     System.out.println("Please enter the width and height of the rectangle");
     double input1=input.nextDouble();
     double input2=input.nextDouble();
     double area=0,Perimeter=0;
-area=input1*input2;
-Perimeter=input1+input2;
+    area=input1*input2;
+    Perimeter=input1+input2;
     System.out.println(area);
     System.out.println(2*Perimeter);
     input.close();
