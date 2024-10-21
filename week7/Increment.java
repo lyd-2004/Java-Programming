3b779c2d294317188249b5b86c8e871bc10e581c
lyd-2004 <3343109033@qq.com>
2024-10-21 14:56:37 +0800

    Create Increment.java

---
+++ week7/Increment.java
@@ -0,0 +1,12 @@
+public class Increment {
+    public static void main(String[] args) {
+        int x=1;
+        System.out.println("Before the call,x is "+x);
+        increment(x);
+        System.out.println("After the call,x is "+x);
+    }
+    public static void increment(int x) {
+        x++;
+        System.out.println("x inside the method is "+x);
+    }
+}
