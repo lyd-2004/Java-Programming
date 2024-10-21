e353029e2baaf15089f90bcea4e267c6522dd3eb
lyd-2004 <3343109033@qq.com>
2024-10-21 15:06:39 +0800

    Create TestMethodOverLoading.java

---
+++ week7/TestMethodOverLoading.java
@@ -0,0 +1,16 @@
+public class TestMethodOverLoading {
+    public static void main(String[] args) {
+        System.out.println("The maximum of 3 and 4 is "+max(3,4));
+        System.out.println("The maximum of 3.0 and 5.4 is "+max(3.0,5.4));
+        System.out.println("The maximum of 3.0,5.4,and 10.14 is "+max(3.0,5.4,10.14));
+    }
+    public static int max(int num1,int num2) {
+        return num1>num2?num1:num2;
+    }
+    public static double max(double num1,double num2) {
+        return num1>num2?num1:num2;
+    }
+    public static double max(double num1,double num2,double num3) {
+        return max(max(num1,num2),num3);
+    }
+}
