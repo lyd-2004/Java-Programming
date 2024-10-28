83d3ed7807a634c40e03a541b4153b023b7e3a6d
lyd-2004 <3343109033@qq.com>
2024-10-28 17:38:09 +0800

    Update Week8Task2.java

--- week8/Week8Task2.java
+++ week8/Week8Task2.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class Week8Task2 {
-    public static void main(String[]args){
+    public static void main(String[]args) {
         double[] mylist=new double[5];
         Scanner input=new Scanner(System.in);
         System.out.print("Enter "+mylist.length+" Values:");
-        for(int i=0;i<mylist.length;i++){
+        for(int i=0;i<mylist.length;i++) {
             mylist[i]=input.nextDouble();
         }
         input.close();
