15d44846b0dc84d1603ff8a2ccc799e2d111b6d4
lyd-2004 <3343109033@qq.com>
2024-11-05 00:16:13 +0800

    Update Circle.java

--- week9/Circle.java
+++ week9/Circle.java
@@ -1,16 +1,29 @@
-public class TestSimpleCircle2 {
+public class Circle {
     public static void main(String[] args) {
-        Circle Circle1 = new Circle();
-        System.out.println("The area of the Circle of radius " + Circle1.radius + " is " + Circle1.getArea());
-
-        Circle Circle2 = new Circle(25); // object-2
-        System.out.println("The area of the Circle of radius " + Circle2.radius + " is " + Circle2.getArea());
-
-        Circle Circle3 = new Circle(75); // object-3
-        System.out.println("The area of the Circle of radius " + Circle3.radius + " is " + Circle3.getArea());
-
-        Circle2.radius = 100; // or Circle2.setRadius(100)
+        Circle Circle1=new Circle();
+        System.out.println("The area of the Circle of radius "+Circle1.radius+" is "+Circle1.getArea());
+        Circle Circle2=new Circle(25); 
+        System.out.println("The area of the Circle of radius "+Circle2.radius+" is "+Circle2.getArea());
+        Circle Circle3=new Circle(75); 
+        System.out.println("The area of the Circle of radius "+Circle3.radius+" is "+Circle3.getArea());
+        Circle2.radius=100; 
         Circle3.setRadius(100);
-        System.out.println("The area of the Circle of radius " + Circle2.radius + " is " + Circle2.getArea());
+        System.out.println("The area of the Circle of radius "+Circle2.radius+" is "+Circle2.getArea());
+    }
+    double radius;
+    Circle() {
+        radius=1.0;
+    }
+    Circle(double newRadius) {
+        radius=newRadius;
+    }
+    double getArea() {
+        return Math.PI*radius*radius;
+    }
+     double getPerimeter() {
+        return 2*Math.PI*radius;
+    }
+     void setRadius(double newRadius) {
+        radius=newRadius;
     }
 }
