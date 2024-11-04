08a2d3869c555a72b7d46ec9f10eb6c16d046883
lyd-2004 <3343109033@qq.com>
2024-11-05 00:03:38 +0800

    Create Circle.java

---
+++ week9/Circle1/Circle.java
@@ -0,0 +1,18 @@
+public class Circle {
+    double radius;
+    Circle() {
+        radius=1.0;
+    }
+    Circle(double newRadius) {
+        radius=newRadius;
+    }
+    double getArea() {
+        return Math.PI*radius*radius;
+    }
+     double getPerimeter() {
+        return 2*Math.PI*radius;
+    }
+     void setRadius(double newRadius) {
+        radius=newRadius;
+    }
+}
