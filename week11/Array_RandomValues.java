ee1291e710a728112524b675a82f43d9255a8c55
lyd-2004 <3343109033@qq.com>
2024-11-18 14:45:26 +0800

    Create Array_RandomValues.java

---
+++ week11/Array_RandomValues.java
@@ -0,0 +1,11 @@
+public class Array_RandomValues {
+    public static void main(String[] args) {
+        int [][] a=new int [3][3];
+        for(int i=0;i<a.length;i++) {
+            for(int j=0;j<a[0].length;j++) {
+                a[i][j]=(int)(Math.random()*20);
+            }
+        }
+        System.out.println(a[0][0]);
+    }
+}
