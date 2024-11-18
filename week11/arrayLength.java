43ecac5e7e4cf337c4bed3e4766357c6cf5434d3
lyd-2004 <3343109033@qq.com>
2024-11-18 14:08:56 +0800

    Create arrayLength.java

---
+++ week11/arrayLength.java
@@ -0,0 +1,10 @@
+public class arrayLength {
+    public static void main(String[] args) {
+        int[][] array={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+        System.err.println("array.length= "+array.length);
+        System.err.println("array[0].length= "+array[0].length);
+        System.err.println("array[1].length= "+array[1].length);
+        System.err.println("array[2].length= "+array[2].length);
+        System.err.println("array[3].length= "+array[3].length);
+    }
+}
