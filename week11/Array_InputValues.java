adf1279f8f5e8c22a52076261e27dc93a1008062
lyd-2004 <3343109033@qq.com>
2024-11-18 14:46:56 +0800

    Create Array_InputValues.java

---
+++ week11/Array_InputValues.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Array_InputValues {
+    public static void main(String[] args) {
+        int [][] matrix=new int [3][3];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter values in "+matrix.length+" rows and "+matrix[0].length+" columns");
+        for(int i=0;i<matrix.length;i++) {
+            for(int j=0;j<matrix[0].length;j++) {
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+        sc.close();
+    }
+}
