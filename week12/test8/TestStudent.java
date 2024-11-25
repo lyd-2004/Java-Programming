c83e3e6cf5c5ff1671296a97fa2f6fa94e4915f9
lyd-2004 <3343109033@qq.com>
2024-11-25 13:51:44 +0800

    Create TestStudent.java

---
+++ week12/test8/TestStudent.java
@@ -0,0 +1,9 @@
+package test8;
+public class TestStudent {
+    public static void main(String[] args) {
+        Student student1=new Student();
+        Student student2=new Student();
+        System.out.println("The name of studnet1 is"+student1.getName());
+        System.out.println("The name of studnet2 is"+student2.getName());
+    }
+}
