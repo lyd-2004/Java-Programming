c71141a5da64bbbe51a557a8f299f0128e834922
lyd-2004 <3343109033@qq.com>
2024-11-25 18:24:20 +0800

    Create testStudent.java

---
+++ week12/test7/testStudent.java
@@ -0,0 +1,13 @@
+package test7;
+public class testStudent {
+    public static void main(String[] args) {
+        Student student=new Student("Alice",20);
+        System.out.println("Name:"+student.getName());
+        System.out.println("Age:"+student.getAge());
+        student.setName("Bob");
+        student.setAge(25);
+        student.setAge(-5);
+        System.out.println("Name:"+student.getName());
+        System.out.println("Age:"+student.getAge());
+    }
+}
