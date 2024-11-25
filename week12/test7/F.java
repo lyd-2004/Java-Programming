99db0fcd4d640ef7c41bf9c8b8ac8854a0e25bbf
lyd-2004 <3343109033@qq.com>
2024-11-25 13:55:02 +0800

    Create F.java

---
+++ week12/test7/F.java
@@ -0,0 +1,12 @@
+package test7;
+public class F {
+    private int x=0;
+    private int y=0;
+    public F() {
+    }
+    public void p() {
+        int x=1;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+    }
+}
